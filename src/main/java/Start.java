8af940885c79b012e4000fb37f8c07b93b195140
Zhelezyaka <support@zhelezyaka.kz>
2017-12-09 05:28:53 +0600

    hibernate transactions

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,5 +1,10 @@
+import model.Product;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Vladimir Glinskikh on 04.12.2017.
@@ -9,8 +14,13 @@ public class Start {
     public static void main(String[] args) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
         Session session = sessionFactory.openSession();
+        List<Object> products = null;
         try {
             session.beginTransaction();
+
+            SQLQuery query = session.createSQLQuery("SELECT * FROM product");
+            query.addEntity(Product.class);
+            products = query.list();
 
             session.getTransaction().commit();
         } catch (Exception e) {
@@ -19,6 +29,11 @@ public class Start {
         } finally {
             session.close();
             sessionFactory.close();
+        }
+        for (Iterator iterator = products.iterator();
+             iterator.hasNext(); ) {
+            Product product = (Product) iterator.next();
+            System.out.println(product.toString());
         }
     }
 }
